afafc154b80721667c632897da5fda62c3df5d0d
Carlotta Chen <carlottachen92@gmail.com>
2021-11-09 13:31:28 -0800

    Third implementation working

--- calc.js
+++ calc.js
@@ -7,26 +7,26 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? (ex. 2 + 2)", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
+	mathSymbol = tokens[1];
+	num1 = Number(tokens[0]);
 	num2 = Number(tokens[2]);
 
 	if(mathSymbol === "+"){
 		console.log(num1 + num2);
 	}
-	if(mathSymbol === "-"){
+	else if(mathSymbol === "-"){
 		console.log(num1 - num2);
 	}
-	if(mathSymbol === "/"){
+	else if(mathSymbol === "/"){
 		console.log(num1 / num2);
 	}
-	if(mathSymbol === "*"){
+	else if(mathSymbol === "*"){
 		console.log(num1 * num2);
 	}
-	if(mathSymbol === "sqrt"){
+	else if(mathSymbol === "sqrt"){
 		console.log(Math.sqrt(num1));
 	}
 	else{
